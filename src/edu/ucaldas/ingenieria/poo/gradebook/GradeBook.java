eb67552fcfb30cd14d1b64f931566a63baefd173
Leonardo Montes <leonardo.montesm@gmail.com>
2023-09-13 21:19:46 -0500

    Add Encapsulation getters and setter, Add consutrctors

--- src/edu/ucaldas/ingenieria/poo/gradebook/GradeBook.java
+++ src/edu/ucaldas/ingenieria/poo/gradebook/GradeBook.java
@@ -1,12 +1,39 @@
 package edu.ucaldas.ingenieria.poo.gradebook;
 
 public class GradeBook {
-
 	
-		public void displayMessage(String course)
-		{
-			System.out.println("Hello from GradeBook class " + course);
-		}
-		// TODO Auto-generated constructor stub
+	private String courseName;
+	
+	//Zona de constructores
+	public GradeBook()
+	{
+		
+	}
+		/**
+	 * @param courseName
+	 */
+	public GradeBook(String courseName) {
+		this.courseName = courseName;
+	}
+
+	//Zonas getters y setters
+		/**
+	 * @return the courseName
+	 */
+	public String getCourseName() {
+		return courseName;
+	}
+
+	/**
+	 * @param courseName the courseName to set
+	 */
+	public void setCourseName(String courseName) {
+		this.courseName = courseName;
+	}
+
+	public void displayMessage() {
+		System.out.println("Hello from GradeBook class " + getCourseName());
+	}
+	// TODO Auto-generated constructor stub
 
 }
--- src/edu/ucaldas/ingenieria/poo/gradebook/GradeBookTest.java
+++ src/edu/ucaldas/ingenieria/poo/gradebook/GradeBookTest.java
@@ -14,10 +14,15 @@ public class GradeBookTest {
 		Scanner sc = new Scanner(System.in);
 	    
 		GradeBook gradebook = new GradeBook();
+		GradeBook gradebookProgramacionOO = new GradeBook("Programacion Orientadan a Objetos");
+		
 		
 		System.out.println("Give a course name: ");
 		String courseName = sc.nextLine();
-		gradebook.displayMessage(courseName);
+		gradebook.setCourseName(courseName);
+		System.out.println("nombre del curso desde el Main() :" + gradebook.getCourseName());
+		System.out.println("nombre del curso con constructor nombre" + gradebookProgramacionOO.getCourseName());
+		gradebook.displayMessage();
 	}
 
 }
